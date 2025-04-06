bcc77d07db0fab1e9ca6a365394c574c4a02b2e0
lbuitran <larrylobster303@gmail.com>
2025-04-06 07:52:38 -0700

    added styling to welcome page

--- my-app/src/components/MainPage.js
+++ my-app/src/components/MainPage.js
@@ -4,16 +4,23 @@ import { useNavigate } from 'react-router-dom';
 function MainPage() {
   const navigate = useNavigate();
 
-  return (
-    <div className="main-page">
-      <h1>Welcome</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-      <div className="button-group">
-        <button onClick={() => navigate('/login')}>Login</button>
-        <button onClick={() => navigate('/signup')}>Sign Up</button>
-      </div>
-    </div>
-  );
+    return (
+        <div className="main-page">
+          <div className="left-panel">
+            <img src="/SOLZ.png" alt="Logo" className="logo" />
+            <div className="button-group-main">
+              <button onClick={() => navigate('/login')}>Login</button>
+              <button onClick={() => navigate('/signup')}>Sign Up</button>
+            </div>
+          </div>
+          
+          <div className="right-panel">
+            <p>
+            By logging data or importing data through our easy to use app, we use AI to give you an accurate medical assessment on your wellbeing.
+            </p>
+          </div>
+        </div>
+      );
 }
 
 export default MainPage;
