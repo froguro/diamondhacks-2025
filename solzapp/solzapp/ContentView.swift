292248731329588dd45d5b4c79af4cb3a4e35970
lbuitran <lbuitran@uci.edu>
2025-04-06 03:06:27 -0700

    changed logo

--- solzapp/solzapp/ContentView.swift
+++ solzapp/solzapp/ContentView.swift
@@ -12,16 +12,15 @@ struct ContentView: View {
                 // Title at the top
                 HStack {
                     Spacer()
-                    Image("happy") //replace with logo
+                    Image("SOLZ") //replace with logo
                         .resizable()
-                        .frame(width: 100, height: 100)
+                        .frame(width: 225, height: 125)
                     Spacer()
                 }.padding(.top, 20)
                 
                 Text("Welcome!")
                     .font(.largeTitle)
                     .fontWeight(.bold)
-                    .padding(.top, 40)
                     .padding(.horizontal, 35)
 
                 Text("To get your medical assessment, \n\nStep 1: Allow access to the health app \n\nStep 2: Enter your username\n\nStep 3: Tap the button to go to our assessment website \n\nStep 4: To verify security, we'll send you an authentication code through Auth0")
