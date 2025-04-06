75e2f45f486dde27f49fd3cf1cf32aa949a0840f
lbuitran <lbuitran@uci.edu>
2025-04-05 18:46:04 -0700

    added button

---
+++ solzapp/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  solzapp
+//
+//  Created by Larry Bui Tran on 4/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @Environment(\.openURL) var openURL
+
+    var body: some View {
+        VStack {
+            Button(action: {
+                if let url = URL(string: "https://www.example.com") {
+                    openURL(url)
+                }
+            }) {
+                Text("Go to Website")
+                    .font(.title2)
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(Color.blue)
+                    .cornerRadius(12)
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ solzapp/solzappApp.swift
@@ -0,0 +1,17 @@
+//
+//  solzappApp.swift
+//  solzapp
+//
+//  Created by Larry Bui Tran on 4/5/25.
+//
+
+import SwiftUI
+
+@main
+struct solzappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
